7dc403b853384a202a0c83124fa86035a7b7abed
Pavlukan <theremustbesomethingsmart@gmail.com>
2022-09-30 19:16:13 +0300

    Build a function which creates book card elements based on the amount of book objects

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const library = document.querySelector("main");
+
 let myLibrary = [];
 
 function Book(title, author, pages, status) {
@@ -13,3 +15,13 @@ function addBookToLibrary(title, author, pages, status) {
 
 addBookToLibrary("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 443, "read");
 addBookToLibrary("A Short History of Nearly Everything", "Bill Bryson", 544, "read");
+
+function createBookCardElements() {
+    myLibrary.forEach(book => {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        library.appendChild(bookCard);
+    });
+}
+
+displayBookObjects();
