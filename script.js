b4cbc706d3cb26b5fdaed0955fa477d79c902d62
Pavlukan <theremustbesomethingsmart@gmail.com>
2022-09-30 18:55:13 +0300

    Build a function which pushes new book objects inside myLibrary array

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function Book(title, author, pages, status) {
     this.pages = pages;
     this.status = status;
 }
+
+function addBookToLibrary(title, author, pages, status) {
+    myLibrary.push(new Book(title, author, pages, status));
+}
+
+addBookToLibrary("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 443, "read");
+addBookToLibrary("A Short History of Nearly Everything", "Bill Bryson", 544, "read");
